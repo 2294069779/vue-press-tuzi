37226c99b651625c10510d195abf3410665c1665
YiPeng Guo <>
2023-04-20 17:56:54 +0800

    ·

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,17 +1,13 @@
-// import Loading from '.'
-export default ({ Vue, options, router, siteData }) => {
-  window.onload = () => {
-    const header = document.querySelector(".navbar");
-    let lastScrollTop = header.offsetHeight;
-    window.addEventListener("scroll", () => {
-      const scrollTop =
-        window.pageYOffset || document.documentElement.scrollTop;
-      if (scrollTop > lastScrollTop) {
-        header.classList.add("hidden");
-      } else {
-        header.classList.remove("hidden");
-      }
-      lastScrollTop = scrollTop;
-    });
-  };
-};
+// export default ({ Vue }) => {
+//   const header = document.querySelector(".navbar");
+//   let lastScrollTop = header.offsetHeight;
+//   window.addEventListener("scroll", () => {
+//     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//     if (scrollTop > lastScrollTop) {
+//       header.classList.add("hidden");
+//     } else {
+//       header.classList.remove("hidden");
+//     }
+//     lastScrollTop = scrollTop;
+//   });
+// };

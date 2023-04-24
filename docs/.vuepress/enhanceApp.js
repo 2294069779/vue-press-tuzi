0b492d226f5f825409edd56c2cfa80a86edf50db
YiPengGuo <>
2023-04-24 17:58:03 +0800

    ~修改图片

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,16 +1,21 @@
-export default {
-  mounted() {
-    const header = document.querySelector(".navbar");
-    let lastScrollTop = header.offsetHeight;
-    window.addEventListener("scroll", () => {
-      const scrollTop =
-        window.pageYOffset || document.documentElement.scrollTop;
-      if (scrollTop > lastScrollTop) {
-        header.classList.add("hidden");
-      } else {
-        header.classList.remove("hidden");
-      }
-      lastScrollTop = scrollTop;
-    });
-  },
+export default ({ Vue, options, router, siteData }) => {
+  Vue.mixin({
+    mounted() {
+      setTimeout(() => {
+        const header = document.querySelector(".navbar");
+        let lastScrollTop = header.offsetHeight;
+
+        window.addEventListener("scroll", () => {
+          const scrollTop =
+            window.pageYOffset || document.documentElement.scrollTop;
+          if (scrollTop > lastScrollTop) {
+            header.classList.add("hidden");
+          } else {
+            header.classList.remove("hidden");
+          }
+          lastScrollTop = scrollTop;
+        });
+      }, 0);
+    },
+  });
 };
